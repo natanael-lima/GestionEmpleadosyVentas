3449e4ea41a860e356953e30b7c9577147479089
Mateo <Mateo@DESKTOP-COJPJTC>
2021-05-31 21:09:50 -0300

    Req1 Primer Commit

---
+++ TpFinal-Empresaurios/src/main/java/ar/edu/unju/fi/tpf/TpFinalEmpresauriosApplication.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi.tpf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinalEmpresauriosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpFinalEmpresauriosApplication.class, args);
+	}
+
+	/*Integrantes:
+     * Nahuel Maximiliano Gaitan - "maxi1103"
+     * Natanael Ever Lima Gutierrez - "natanael-lima"
+     * Mateo Ignacio Loto - "LMateo42"
+     * Raquel Ayelen Virreyra Ruiz - "raquelvirreyra"
+     */
+		
+}
---
+++ TpFinal-Empresaurios/src/main/java/ar/edu/unju/fi/tpf/service/Hola.java
@@ -0,0 +1,5 @@
+package ar.edu.unju.fi.tpf.service;
+
+public class Hola {
+
+}
